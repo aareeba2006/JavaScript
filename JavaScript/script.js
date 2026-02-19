4bb060355dc4a1ac5832fed4207f21bb2b9d5c04
aareeba2006 <aareeba2006@gmail.com>
2026-02-20 03:21:39 +0500

    Add files via upload

---
+++ JavaScript/script.js
@@ -0,0 +1,226 @@
+//Chapter 1 Question 1
+// var greet= "Hello World"
+// alert(greet)
+
+// Question 2
+// var message="Error! Please enter a valid password"
+// alert(message)
+
+// Question 3
+// var message= "Welcome to JS Land \n Happy Coding"
+// alert (message)
+
+// Question 4
+// var greet= "Welcome to JS Land"
+// alert(greet)
+// var message= "Prevent this page from creating additional dialogs"
+// alert(message)
+
+//Question 5
+
+
+
+
+// Chapter 2 Question 1 & 2
+// var username;
+// var myName= "Areeba Ilyas"
+
+// Question 3
+// var message= "Hello World"
+// alert(message)
+
+//  Question 4
+// var fullName= "John Doe"
+// alert(fullName)
+// var age= 15
+// alert(age + " years old")
+// var course= "Certified Mobile App Development"
+// alert(course)
+
+// Question 5
+// var word= "Pizza \n Pizz \n Piz \n Piz \n Pi \n P"
+// alert(word)
+
+// Question 6
+// var email= "areebayilyas@gmail.com"
+// alert("My email address is " + email)
+
+// Question 7
+// var book= "A smarter way to learn JavaScript"
+// alert("I am trying to learn from the Book " + book)
+
+// Question 8
+// var message= "Yah! I can write HTML content through JavaScript"
+// document.write(message)
+
+// Question 9
+// var design= "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// document.write(design)
+
+// Chapter 3 Question 1 
+// var age= 15
+// alert(" I am " + age + " years old")
+
+// Question 3 
+// var birthYear= 2007
+// document.write("My birthyear is " + birthYear + "<br>" + "Data type of my declared variable is numbered")
+
+//Question 4
+// var visitorName= "John Doe"
+// var productTitle= "T-shirt(s)"
+// var Quantity= 5
+// document.write(visitorName + " ordered" + " " + Quantity + productTitle + " on XYZ Clothing Store")
+
+// chapter 4 Question 1
+// var p = "apple", q = "banana", r = "cherry"; 
+
+// Question 2
+// Legal variables
+// var age = 25;
+// var userName = "Areeba";
+// var $price = 100;
+// var _count = 50;
+// var totalAmount123 = 5000;
+// Illegal variables
+// var 2ndUser = "John";      
+// var user-name = "Mike";    
+// var @amount = 200;         
+// var var = 10;           
+// var my variable = "Test";  
+
+// Question 3
+// var heading= "Rules for naming JS variables"
+// document.write("<h1>" + heading + "</h1>")
+// var writing= "Variables can only contain, number,$ and _.For example: $my_1stvariable"
+// document.write(writing)
+// var legal= "Variables must begin with a letter,$ number,$ or _.For example: $name,_name or name"
+// document.write("<br>" + legal)
+// var allow= "Variables names are case sensitive"
+// document.write("<br>" + allow)
+// var illegal= "Variables names should not be JS keywords"
+// document.write("<br>" + illegal)
+
+// Chapter 5 question 1
+// var num1= 3
+// var num2= 5
+// var sum= num1+num2
+// document.write(sum)
+
+// Question 2
+// var num1= 3
+// var num2= 5
+// var sub= num1-num2
+// document.write("<br>"+ sub)
+// var num1= 3
+// var num2= 5
+// var mult= num1*num2
+// document.write("<br>"+ mult)
+// var num1= 3
+// var num2= 5
+// var div= num1/num2
+// document.write("<br>"+ div)
+// var num1= 3
+// var num2= 5
+// var mod= num1%num2
+// document.write("<br>"+ mod)
+
+// Question 3
+// var num;
+// document.write("Value after variable declaration is: " + num )
+// num=5
+// document.write("<br>" + "Initial value: " + num)
+// num=num+1
+// document.write("<br>" + "After increment value: " + num)
+// num=num+7
+// document.write("<br>" + "Value after addition is: " + num)
+// num=num-1
+// document.write("<br>" + "After decrement value: " + num)
+// num=num%3
+// document.write("<br>" + "The remainder is : " + num)
+
+// Question 4
+// var ticketPrice=600
+// ticketPrice *= 5
+// document.write("<br>" + "Total cost to buy 5 tickets to a movie is : " + ticketPrice+"PKR" )
+
+// Question 5
+
+
+
+
+// Question 6
+// var celsius=30
+// var F= (celsius * 9/5) + 32
+// document.write(F + "°F")
+// var fahrenheit=86
+// var C=(fahrenheit - 32) * 5/9
+// document.write("<br>" + C + "°C")
+
+// Question 7
+// var writing="Shopping Cart"
+// document.write("<b>" + writing + "</b>" + "<br>") 
+// var item1= 650
+// document.write( "<br>" + "Price of item 1= " + item1)
+// var Quantity_1=3
+// var Item1;
+// Item1=item1*Quantity_1
+// document.write("<br>" + "Ordered quantity of item 1= " + Quantity_1)
+// var item2= 100
+// document.write( "<br>" + "Price of item 2= " + item2)
+// var Quantity_2=7
+// document.write("<br>" + "Ordered quantity of item 2= " + Quantity_2)
+// var Item2;
+// Item2=item2*Quantity_2
+// var shippingCharges=100
+// var totalCost;
+// totalCost=Item1+Item2+shippingCharges
+// document.write("<br>" + "Total cost of your order is= " + totalCost)
+
+// Question 8
+// var totalMarks=980
+// var obtainedMarks=804
+// var percentage=obtainedMarks * 100 /totalMarks
+// document.write("Percentage: " + percentage)
+
+// Question 9
+var dollars=10*104.80
+var riyal=25*28
+var rupee=dollars+riyal
+document.write("Pakistani Rupee is: " + rupee)
+
+
+
+
+
+
+// Question 10
+// var num=5
+// num=num+5
+// document.write(num)
+// num=num*10
+// document.write("<br>" + num)
+// num=num/2
+// document.write("<br>" + num)
+
+// Question 11
+// var currentYear=2026
+// var birthYear=2007
+// var age=currentYear - birthYear
+// document.write("Current Year: " + currentYear + "<br>" + "Birth Year: " + birthYear + "<br>" + " Age: " +  age)
+
+// Question 12
+// var radius=20
+// var circumference=2*3.142*radius
+// var area=3.142*radius*radius
+// document.write("Radius of a circle is : " + radius + "<br>" + "Circumference is: " + circumference + "<br>" + "Area is: " + area)
+
+// Question 13
+// var snack="Chocolate Chip"
+// var currentAge=15
+// var maxAge=65
+// var day=3
+// var age1=day*maxAge
+// var age2=day*currentAge
+// var age=age1-age2
+// document.write("Snack is:" + snack + "<br>"+ "Current Age: " + currentAge +" Max Age:"+ maxAge+"<br>"+"Amount of snacks per day: "+ day)
+// document.write("<br>"+ "You will need " + age + " to last until you rip old age of " + maxAge)
