865e910b5d05b18ce91c6c5bc039f08acef4b8e6
aareeba2006 <aareeba2006@gmail.com>
2026-04-03 04:53:30 +0500

    JavaScript Chapter 18-20

---
+++ JavaScript Chap 18-20/script.js
@@ -0,0 +1,80 @@
+//Chapter 18-20
+// Questoin 1
+// arr=[[],[],[]];
+
+// Question 2
+// var  array=[[0,1,2,3],[1,0,1,2],[2,1,0,1]];
+// for(var i=0; i<array.length; i++){
+//     console.log(array[i]);
+// }
+
+// Question 3
+// for(var i=1; i<=10; i++){
+//     console.log(i);
+// }
+
+// Question 4
+// var table=prompt("Enter a number: ");
+// var tablelength=prompt("Enter length: ");
+// for (var i=1; i<=tablelength; i++){
+//     console.log(table + "x" + i + "=" + table*i);
+// }
+
+// Question 5
+// var fruits=["apple","banana","mango","orange","strawberry"];
+// for( var i=0; i<fruits.length; i++){
+//     console.log(fruits[i]);
+//     console.log("Element at index " + i + " is " + fruits[i]);
+// }
+
+// Question 6
+// for (var i=1; i<=10; i++){
+//     console.log("Counting: " + i);
+// }
+// for (var i=10; i>=1; i--){
+//     console.log("Reverse Counting: " + i);
+// }
+// for (var i=0; i<=10; i++){
+//     console.log("Even:" + i*2);
+// }
+// for (var i=1; i<=10; i++){
+//     console.log("Odd: " + (i*2-1));
+// }
+// for (var i=1; i<=10; i++){
+//     console.log("Series: " + (i*2) + "k");
+// }
+
+// Question 7
+// var A=["cake","apple pie","cookie","chips","patties"];
+// var search=prompt("Welcome to ABC Bakery. What do you want to order sir/ma'am?");
+// for (var i=0; i<A.length; i++){
+//     if(A[i]===search){
+//         console.log(search + " is available at index " + i + " in our bakery");
+//     }
+//     else{
+//         console.log("We are sorry. " + search + " is not available in our bakery");
+//     }
+// }
+
+// Question 8
+// var A=[24,53,78,91,12];
+// for( var i=0; i<A.length; i++){
+//     if(A[i]>A[0]){
+//         A[0]=A[i];
+//     }
+// }
+// console.log("The largest number is " + A[0]);
+
+// Question 9
+// var A=[24,53,78,91,12];
+// for( var i=0; i<A.length; i++){
+//     if(A[i]<A[0]){
+//         A[0]=A[i];
+//     }
+// }
+// console.log("The smallest number is " + A[0]);
+
+// Question 10
+// for(var i=1; i<=20; i++){
+//     console.log(i*5);
+// }
